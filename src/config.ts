471e8a92b643548ee2d01d733ecf6478f4784e92
pauloli <616752336@qq.com>
2023-06-04 09:34:04 +0800

    feat: update userInfo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Modern Personal Website Template';
-export const SITE_DESCRIPTION = 'Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
+export const SITE_TITLE = "叉烧君的学习日记";
+export const SITE_DESCRIPTION =
+  "前端学习笔记和文章分享，记录我的前端学习心得和经验，探索Web开发的无限可能。在这里，我与你分享前端技术的学习过程、解决方案、最佳实践以及对新兴技术的探索。让我们一起在前端的世界中不断成长和进步！";
